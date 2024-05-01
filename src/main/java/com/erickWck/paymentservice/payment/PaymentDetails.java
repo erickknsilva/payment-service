4f4ff1a59c542a7fa2fd3ab286942b8567caf177
erickknsilva <erick.nsilva100@gmail.com>
2024-05-01 03:33:39 -0300

    Esse serviço é responsavel por simular um API de pagamento recebendo os dados simulado de um cartão, fazer as validação e salvar no banco de dados caso seja aprovado

--- src/main/java/com/erickWck/paymentservice/payment/PaymentDetails.java
+++ src/main/java/com/erickWck/paymentservice/payment/PaymentDetails.java
@@ -1,21 +1,43 @@
 package com.erickWck.paymentservice.payment;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
 public record PaymentDetails(
 
 
+        @NotNull(message = "Insira o valor total do produto.")
+        @Positive(message = "O preço deve ser um número positovo.")
         BigDecimal amount,
 
+
+        @NotBlank(message = "Insira o tipo do cartão credito ou debito")
         String type,
 
+
+        @NotBlank(message = "Insira o numero do cartão")
+        @Pattern(regexp = "^([0-9]{16})$", message = "O número do cartão está invalido.")
         String cardNumber,
 
+
+        @NotBlank(message = "Insira a data de expiração")
+        @Pattern(regexp = "^([0-9]{6})$", message = "A data de expiração deve conter só 6 numeros.")
         String expiryDate,
 
+        @NotBlank(message = "Insira o código de segurançã do cartão")
+        @Pattern(regexp = "^([0-9]{3})$", message = "O número do cartão está invalido.")
         String cvv,
+
+
+        @NotBlank(message = "Insira o nome do responsavel pelo cartão.")
         String cardholderName,
 
+
+        @NotNull(message = "Insira o número do pedido")
         Long orderId
 
 ) {
